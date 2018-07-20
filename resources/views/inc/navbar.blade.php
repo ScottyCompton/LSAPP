7c050d6bcac1a73143ca89b2845bc6c2cb4ca5b1
Scotty Lonis <techguru4hire@gmail.com>
2018-07-20 10:36:42 -0600

    Initial Commit

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">{{config('app.name', 'LSAPP')}}</a>
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/services">Servies</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/posts">Blog</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
